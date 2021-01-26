f489f20b95fb831dfcec85870e12b4e3fc6c1d9c
Olesea1986 <olesea86williams@gmail.com>
2021-01-26 12:43:13 -0600

    B21-commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 
 
         System.out.println("Hello World");
+        System.out.println("In Chicago is snowing");
+        System.out.println("Love to be happy, to have positive thoughts and vibes");
     }
 }
